55a697ecc807b38a23385d35b0c94ed486c8095c
TheLuisAbarca <arclaav.adm@gmail.com>
2022-02-24 16:24:57 -0500

    Implement our_each method for MyList Class file.

--- my_list.rb
+++ my_list.rb
@@ -6,11 +6,22 @@ class MyList
     @list = nums
   end
 
-  def each
+  def our_each
+    # @list.each { |num| yield num }
+    return to_enum(:our_each) unless block_given?
 
+    i = 0
+    size = @list.size
+    while i < size
+      is_a?(Range) ? yield(min + i) : yield(@list[i])
+      i += 1
+    end
+    @list
   end
 end
 
-list = MyList.new(1,2,3,4)
+list = MyList.new(1,2,3,4,5,6,7,8,9,10)
 
 puts list
+puts list.our_each { |num| num * 2 }
+puts list.our_each
